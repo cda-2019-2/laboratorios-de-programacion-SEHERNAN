cb9414a15295959f75432c2f89c21e4fb793ca83
SEHERNAN <67772563+SEHERNAN@users.noreply.github.com>
2020-08-04 23:45:16 -0500

    Update question.py

--- 03-python=1/q01=1/question.py
+++ 03-python=1/q01=1/question.py
@@ -8,5 +8,13 @@
 ##  190
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
-##
+archivo = open('data.csv','r')
+columna2 = []
+ 
+for line in archivo:
+   x = line.strip()
+   y = x.split()
+   columna2.append(int(y[1]))
+archivo.close()
+print(sum(columna2))
 
