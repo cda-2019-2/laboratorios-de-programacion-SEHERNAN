0cd5f3ae6ba983865cc8f0abae79d38dc312251e
SEHERNAN <67772563+SEHERNAN@users.noreply.github.com>
2020-08-06 01:19:46 -0500

    Update question.py

--- 04-pandas=1/q09=1/question.py
+++ 04-pandas=1/q09=1/question.py
@@ -15,5 +15,17 @@
 ##  39   39    a,d,f
 ## 
 ##  >>> Escriba su codigo a partir de este punto <<<
-##
+import numpy as np
+import pandas as pd
+import csv
+
+
+separador=','
+
+datos= pd.read_csv('tbl1.tsv', sep='\t', header=0 )
+#datos['_c2']=datos['_c2'].apply(str)
+datos.sort_values(['_c0','_c4'],inplace=True)
+Tabla=datos[['_c0','_c4']].groupby('_c0')['_c4'].apply((separador.join)).reset_index()
+Tabla.rename(columns={'_c4':'lista'},inplace=True)
+print(Tabla)
 
