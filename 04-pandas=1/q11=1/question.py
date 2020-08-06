d3fa523ee30b03732a2ab9a0a38621ecd3ad992a
SEHERNAN <67772563+SEHERNAN@users.noreply.github.com>
2020-08-06 01:19:54 -0500

    Update question.py

--- 04-pandas=1/q11=1/question.py
+++ 04-pandas=1/q11=1/question.py
@@ -16,7 +16,19 @@
 ##  Name: _c5b, dtype: int64
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
-##
+#! /usr/local/bin/python3
+import numpy as np
+import pandas as pd
+import csv
+
+datos= pd.read_csv('tbl2.tsv', sep='\t', header=0 )
+datos1= pd.read_csv('tbl0.tsv', sep='\t', header=0 )
+datos.sort_values(['_c0','_c5b'],inplace=True)
+Tabla=datos[['_c0','_c5b']].groupby('_c0')['_c5b'].apply(lambda x: x.sum()).reset_index()
+Tablam=pd.merge(datos1,Tabla,on='_c0')
+Tablam.sort_values(['_c1','_c5b'],inplace=True)
+Tablaf=Tablam.groupby("_c1")["_c5b"].sum()
+print(Tablaf)
 
 
 
