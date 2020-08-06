78ecc37a388372a9cabe46307cf17e9f483cb2e5
SEHERNAN <67772563+SEHERNAN@users.noreply.github.com>
2020-08-05 23:37:23 -0500

    Update question.py

--- 03-python=1/q11=1/question.py
+++ 03-python=1/q11=1/question.py
@@ -15,5 +15,58 @@
 ##  g,35
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
-##
+f = open('data.csv','r')
+ 
+lista = []
+ 
+#ingreso los registros a una lista para leerlos por linea
+for line in f:
+    y=line.strip()
+    z=y.split()
+    lista.append(z)
+ 
+#Busco la existencia del caracter y luego guardo el valor de la columna 2 en una lista
+a = []
+for line in range(len(lista)):
+    
+    if lista[line][3].count('a') > 0:
+        a.append(int(lista[line][1]))
+ 
+b = []
+for line in range(len(lista)):
+    if lista[line][3].count('b') > 0:
+        b.append(int(lista[line][1]))
+ 
+c = []
+for line in range(len(lista)):
+    if lista[line][3].count('c') > 0:
+        c.append(int(lista[line][1]))
+ 
+d = []
+for line in range(len(lista)):
+    if lista[line][3].count('d') > 0:
+        d.append(int(lista[line][1]))
+ 
+e = []
+for line in range(len(lista)):    
+    if lista[line][3].count('e') > 0:
+        e.append(int(lista[line][1]))
+ 
+f = []
+for line in range(len(lista)):
+    if lista[line][3].count('f') > 0:
+        f.append(int(lista[line][1]))
+ 
+g = []
+for line in range(len(lista)):
+    if lista[line][3].count('g') > 0:
+        g.append(int(lista[line][1]))
+ 
+print('a,',sum(a),sep='')
+print('b,',sum(b),sep='')
+print('c,',sum(c),sep='')
+print('d,',sum(d),sep='')
+print('e,',sum(e),sep='')
+print('f,',sum(f),sep='')
+print('g,',sum(g),sep='')
 
