4b959617bef55e6889a91b4b4df8c625412fc3f2
SEHERNAN <67772563+SEHERNAN@users.noreply.github.com>
2020-08-05 23:37:14 -0500

    Update question.py

--- 03-python=1/q09=1/question.py
+++ 03-python=1/q09=1/question.py
@@ -18,7 +18,64 @@
 ##  jjj,18
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
-##
+f = open('data.csv','r')
+ 
+lista = []
+ 
+countaaa = 0
+countbbb = 0
+countccc = 0
+countddd = 0
+counteee = 0
+countfff = 0
+countggg = 0
+counthhh = 0
+countiii = 0
+countjjj = 0
+ 
+for line in f:
+    x=line.replace(',',' ')
+    x=x.replace(':',' ')
+    y=x.strip()
+    z=y.split()
+ 
+  
+    lista.append(z[3:])
+#print(lista)
+ 
+for i in range(len(lista)):
+    for j in range(len(lista[i])-1):
+        if lista[i][j] == ('aaa'):
+            countaaa += 1
+        elif lista[i][j] == ('bbb'):
+            countbbb += 1
+        elif lista[i][j] == ('ccc'):
+            countccc += 1
+        elif lista[i][j] == ('ddd'):
+            countddd += 1
+        elif lista[i][j] == ('eee'):
+            counteee += 1
+        elif lista[i][j] == ('fff'):
+            countfff += 1
+        elif lista[i][j] == ('ggg'):
+            countggg += 1
+        elif lista[i][j] == ('hhh'):
+            counthhh += 1
+        elif lista[i][j] == ('iii'):
+            countiii += 1
+        elif lista[i][j] == ('jjj'):
+            countjjj += 1
+ 
+print('aaa,',countaaa,sep='')
+print('bbb,',countbbb,sep='')
+print('ccc,',countccc,sep='')
+print('ddd,',countddd,sep='')
+print('eee,',counteee,sep='')
+print('fff,',countfff,sep='')
+print('ggg,',countggg,sep='')
+print('hhh,',counthhh,sep='')
+print('iii,',countiii,sep='')
+print('jjj,',countjjj,sep='')
 
 
 
