5465c596d7ef8d651b79903fee2525989b3ade5e
SEHERNAN <67772563+SEHERNAN@users.noreply.github.com>
2020-08-05 22:37:53 -0500

    Update question.sh

--- 01-sed=1/q01=1/question.sh
+++ 01-sed=1/q01=1/question.sh
@@ -34,4 +34,19 @@
 ##  2014-09-01,A,3,100.4
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
-##
+#!/usr/local/bin/bash
+sed 's/,/./g' < data.csv > data1.csv;
+sed 's/;/,/g' < data1.csv > data2.csv;
+sed ' 12,12 s/\([0-9]\)\//0\1\//' < data2.csv > data3.csv;
+sed ' 12,12 s/\/\([0-9]\)\//\/0\1\//' < data3.csv > data4.csv;
+sed 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' < data4.csv > data5.csv;
+sed 's/,N/\,\\N/g' < data5.csv > data6.csv;
+sed -e 's/\s\{1,\}/,/g' < data6.csv > data7.csv;
+sed 's/,,/\,\\N\,/g' < data7.csv > data8.csv;
+sed 's/,,/\,\\N\,/g' < data8.csv > data9.csv;
+sed 's/,//4' < data9.csv > data10.csv;
+sed '12,12 s/2014/14/g' < data10.csv > data11.csv;
+sed 's/\([0-9][0-9]\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)/20\3-\2-\1/g' < data11.csv > data12.csv;
+sed 's/\//\-/1' < data12.csv > data13.csv;
+sed 's/\//\-/1' < data13.csv > data14.csv;
+cat data14.csv;
