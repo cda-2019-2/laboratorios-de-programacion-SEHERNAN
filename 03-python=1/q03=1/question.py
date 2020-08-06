6fea98e98f613ed47d7549b495d492e27a007e52
SEHERNAN <67772563+SEHERNAN@users.noreply.github.com>
2020-08-05 23:29:49 -0500

    Update question.py

--- 03-python=1/q03=1/question.py
+++ 03-python=1/q03=1/question.py
@@ -13,5 +13,27 @@
 ##  E,67
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
-##
+
+archivo = open('data.csv','r').readlines()
+ 
+#A = [[row[0],row[2]] for row in archivo if row[0] == 'A']
+A2 = [int(str(row[2])) for row in archivo if row[0] == 'A']
+ 
+#a2 = int(str(A2))
+B2 = [int(str(row[2])) for row in archivo if row[0] == 'B']
+C2 = [int(str(row[2])) for row in archivo if row[0] == 'C']
+D2 = [int(str(row[2])) for row in archivo if row[0] == 'D']
+E2 = [int(str(row[2])) for row in archivo if row[0] == 'E']
+ 
+def sumalista(listaNumeros):
+   laSuma = 0
+   for i in listaNumeros:
+       laSuma = laSuma + i
+   return laSuma
+ 
+print('A,'+(str(sumalista(A2))))
+print('B,'+(str(sumalista(B2))))
+print('C,'+(str(sumalista(C2))))
+print('D,'+(str(sumalista(D2))))
+print('E,'+(str(sumalista(E2))))
 
