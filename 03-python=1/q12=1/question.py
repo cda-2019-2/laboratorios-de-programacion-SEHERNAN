0b35d6668c54d64091be3fde7092ed0d14a94acb
SEHERNAN <67772563+SEHERNAN@users.noreply.github.com>
2020-08-05 23:37:28 -0500

    Update question.py

--- 03-python=1/q12=1/question.py
+++ 03-python=1/q12=1/question.py
@@ -15,7 +15,38 @@
 ##  E,16
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
-##
+f = open('data.csv','r')
+#listas temporales para extraer los datos
+lista = []
+temporal = []
+temporal2 = []
+temporal3 = []
+temporal4 = []
+#separar las claves con ':'
+for line in f:
+    
+    x=line.replace(':',',: ')
+    x=x.replace(',',' ')
+    y=x.strip()
+    z=x.split()
+    
+    lista.append(z)
+#extraer la columna 5
+for line in range(len(lista)):
+    temporal.append(lista[line][4:])
+for line in range(len(temporal)):
+    temporal2.append(temporal[line])
+#extraer los valores numericos de las claves
+for i in range(len(temporal2)):
+    for j in range(len(temporal2[i])):
+        if temporal2[i][j] == ':':
+            temporal3.append(int(temporal2[i][j+1]))
+        
+    temporal4.append(sum(temporal3))
+    temporal3 = []
+#suma de los valores numericos de las claves con su valor de la columna 0  
+for line in range(len(lista)):
+    print(lista[line][0],',',temporal4[line],sep='')
 
 
 
