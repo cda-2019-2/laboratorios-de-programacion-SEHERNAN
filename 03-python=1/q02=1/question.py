40faa965e314303cd9f3381bf908429f07e4b48b
SEHERNAN <67772563+SEHERNAN@users.noreply.github.com>
2020-08-05 23:25:13 -0500

    Update question.py

--- 03-python=1/q02=1/question.py
+++ 03-python=1/q02=1/question.py
@@ -13,5 +13,21 @@
 ##  E,14
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
-##
+archivo = open('data.csv','r').readlines()
+ 
+A = [row[0] for row in archivo if row[0] == 'A']
+B = [row[0] for row in archivo if row[0] == 'B']
+C = [row[0] for row in archivo if row[0] == 'C']
+D = [row[0] for row in archivo if row[0] == 'D']
+E = [row[0] for row in archivo if row[0] == 'E']
+AA=str(len(A))
+BB=str(len(B))
+CC=str(len(C))
+DD=str(len(D))
+EE=str(len(E))
+print('A,'+AA)
+print('B,'+BB)
+print('C,'+CC)
+print('D,'+DD)
+print('E,'+EE)
 
