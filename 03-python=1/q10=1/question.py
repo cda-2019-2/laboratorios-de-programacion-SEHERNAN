3d24c72ed36ea6807cc19f88356f293230203849
SEHERNAN <67772563+SEHERNAN@users.noreply.github.com>
2020-08-05 23:37:19 -0500

    Update question.py

--- 03-python=1/q10=1/question.py
+++ 03-python=1/q10=1/question.py
@@ -16,6 +16,21 @@
 ##  E,3,3
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
-##
+f = open('data.csv','r')
+ 
+lista = []
+temporal = []
+ 
+for line in f:
+    y=line.strip()
+    z=y.split()
+    lista.append(z)
+ 
+for line in range(len(lista)):
+    temporal.append([lista[line][0],lista[line][3].count(',')+1,lista[line][4].count(',')+1])
+ 
+for line in range(len(temporal)):
+    print(temporal[line][0],',',temporal[line][1],',',temporal[line][2],sep='') 
+
 
 
