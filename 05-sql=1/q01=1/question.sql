07d0e77405ed606a987be663a8fa6363bbba0984
SEHERNAN <67772563+SEHERNAN@users.noreply.github.com>
2020-08-06 22:04:07 -0500

    Update question.sql

--- 05-sql=1/q01=1/question.sql
+++ 05-sql=1/q01=1/question.sql
@@ -19,40 +19,7 @@
 --  0  15137.63
 --
 --  >>> Escriba su codigo a partir de este punto <<<
-import sqlite3
-conn = sqlite3.connect('data.db')
-cur = conn.cursor()
-
-conn.executescrip("""
-DROP TABLE IF EXISTS data;
-
-CREATE TABLE data (
-    K0  CHAR(1),   
-    K1  INT,        
-    c12 FLOAT,
-    c13 INT,
-    c14 DATE,
-    c15 FLOAT,
-    c16 CHAR(4));
-
-""")
-conn.commit()
-
-with open ('data.csv','rt') as f:
-    data = f.readlines()
-
-data [line[:-1]if line[-1] == '\n' else line for line in data]
-
-data = [line.split(',') for line in data]
-
-data = [tuble(line) for line in data]
-
-cur.executemany(INSERT INTO data VALUES (?,?,?,?,?,?,?), data)
-
-cur.execute ("SELECT SUM(C12) FROM data;").fetchall()
-
-
-
+SELECT SUM(c12) FROM tbl;
 
 
 
