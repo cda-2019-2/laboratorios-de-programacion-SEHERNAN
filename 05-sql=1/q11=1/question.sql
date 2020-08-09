52946d7542a8090c552433659e63a716d62bbafe
SEHERNAN <67772563+SEHERNAN@users.noreply.github.com>
2020-08-08 19:16:46 -0500

    Update question.sql

--- 05-sql=1/q11=1/question.sql
+++ 05-sql=1/q11=1/question.sql
@@ -40,4 +40,4 @@
 --  0  2018     6
 --
 --  >>> Escriba su codigo a partir de este punto <<<
---  
+SELECT strftime('%Y',c14) as "YEAR", COUNT(*) as "CANT" FROM tbl1 WHERE strftime('%Y',c14) = 2018;
